e5bbbb4a27fb78e4260da2e28eb307c5e5c7e4f2
lt <1029563229@qq.com>
2020-05-12 09:31:22 +0800

    Angular 接入 single-spa

---
+++ micro-app-angular/src/app/empty-route/empty-route.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-route',
+  template: '',
+})
+export class EmptyRouteComponent {}
---
+++ micro-app-angular/src/main.single-spa.ts
@@ -0,0 +1,36 @@
+
+import { enableProdMode, NgZone } from '@angular/core';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Router } from '@angular/router';
+import { ɵAnimationEngine as AnimationEngine } from '@angular/animations/browser';
+
+import { singleSpaAngular, getSingleSpaExtraProviders } from 'single-spa-angular';
+
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { singleSpaPropsSubject } from './single-spa/single-spa-props';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+if (!(window as any).__POWERED_BY_QIANKUN__) {
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+}
+
+const { bootstrap, mount, unmount } = singleSpaAngular({
+  bootstrapFunction: singleSpaProps => {
+    singleSpaPropsSubject.next(singleSpaProps);
+    return platformBrowserDynamic(getSingleSpaExtraProviders()).bootstrapModule(AppModule);
+  },
+  template: '<app-root />',
+  Router,
+  NgZone,
+  AnimationEngine,
+});
+
+export { bootstrap, mount, unmount };
